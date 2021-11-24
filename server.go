c3a62e34db1c2de3f5dcc9984e8d727f111cf4f9
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-11-24 23:19:32 +0900

    cors관련 오류 수정

--- server.go
+++ server.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(JSONMiddleware())
+	r.Use(CORSMiddleware())
 
 	r.GET("/books", controllers.FindBooks)
 	r.POST("/books", controllers.CreateBook)
@@ -28,12 +29,29 @@ func main() {
 	// r.GET("/path/:name/:age", PathParameter)
 
 	models.ConnectDatabase()
+
 	r.Run(":4444")
 }
 
 func JSONMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Next()
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithError(204, gin.Error{})
+			return
+		}
+
 		c.Next()
 	}
 }
