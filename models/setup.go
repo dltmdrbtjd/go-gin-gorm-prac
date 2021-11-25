c66e08e119469e95468766b96a4b4bfa3f68c80f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-11-25 23:54:41 +0900

    create db connect env

--- models/setup.go
+++ models/setup.go
@@ -17,8 +17,12 @@ func ConnectDatabase() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	dbUserName := os.Getenv("DB_ID")
+	dbPassWord := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_ID"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUserName, dbPassWord, dbHost, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("connected db failed")
